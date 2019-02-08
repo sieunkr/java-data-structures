50c3212e9f6acd28da4a4984be8280e1f6f62749
sieunkim <sieunkr@gmail.com>
2019-02-08 21:35:37 +0900

    Arrays binarySearch

--- basic/chap03/src/test/java/com/example/demo/BinarySearchTest.java
+++ basic/chap03/src/test/java/com/example/demo/BinarySearchTest.java
@@ -22,25 +22,30 @@ public class BinarySearchTest {
         arr[6] = 77;
         */
         int[] arr = { 1, 5, 7, 9, 40, 55, 77 };
-    
+
         Assert.assertEquals(0, binarySearch(arr, 7, 1));
         Assert.assertEquals(3, binarySearch(arr, 7, 9));
         Assert.assertEquals(5, binarySearch(arr, 7, 55));
         Assert.assertEquals(6, binarySearch(arr, 7, 77));
+        Assert.assertEquals(-1, binarySearch(arr, 7, 99));
+
+
+        //Arrays.binarySearch
+        Assert.assertEquals(6, Arrays.binarySearch(arr, 77));
+        Assert.assertNotEquals(-1, Arrays.binarySearch(arr, 99));
     }
 
     static int binarySearch(int[] arr, int n, int key){
-        int middle;                //가운데 인덱스
-        int left = 0;           //첫 인덱스
-        int right = n - 1;      //끝 인덱스
+        int middle;                     //가운데 인덱스
+        int left = 0;                   //첫 인덱스
+        int right = n - 1;              //끝 인덱스
 
         while(right >= left){
 
             middle = (right + left) / 2;
 
             if(key == arr[middle]){
-                //검색 성공
-                return middle;
+                return middle;          //검색 성공
             }
             else if(arr[middle] < key){
                 left = middle + 1;
@@ -49,7 +54,7 @@ public class BinarySearchTest {
                 right = middle - 1;
             }
         }
-        return -1;  //검색실패
+        return -1;                      //검색실패
     }
 
 }
