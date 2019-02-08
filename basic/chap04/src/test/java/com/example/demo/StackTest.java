29da1a3e6fb0e852ee65275c5e5e08c8d5e8647d
sieunkim <sieunkr@gmail.com>
2019-02-08 22:53:09 +0900

    JDK API 스택

--- basic/chap04/src/test/java/com/example/demo/StackTest.java
+++ basic/chap04/src/test/java/com/example/demo/StackTest.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Stack;
+
 public class StackTest {
 
     @Test
-    public void test(){
+    public void test_custom_stack(){
 
         SimpleStack<String> myStack = new SimpleStack<>(100);
 
@@ -16,11 +18,22 @@ public class StackTest {
 
         Assert.assertEquals(3, myStack.size());
         Assert.assertEquals("테스트", myStack.peek());
-        
+
         Assert.assertEquals("테스트", myStack.pop());
         Assert.assertNotEquals("테스트", myStack.peek());
         Assert.assertEquals(2, myStack.size());
         Assert.assertEquals("아이린", myStack.peek());
+
+    }
+
+    @Test
+    public void test_jdk_stack(){
+        Stack<String> stack = new Stack<>();
+        stack.push("에디킴");
+        stack.push("아이린");
+
+        Assert.assertEquals(2, stack.size());
+        Assert.assertEquals("아이린", stack.peek());
 
     }
 }
